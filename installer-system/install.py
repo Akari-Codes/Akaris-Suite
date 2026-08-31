e57d83770c1590241132de6dc1c610f0c291c6f5
Akari_VT <astilcodex6996@gmail.com>
2026-08-31 13:41:48 -0700

    s

--- installer-system/install.py
+++ installer-system/install.py
@@ -4,16 +4,13 @@ from pathlib import Path
 import joblib
 import py7zr
 import webview
-
+tos = """"""
+config = {"name":"","body":"","tos":tos,"path":"","version":"","author":"","finish":"","fail":""}
 def startUp():
-    config = """"""
     leftPanel = """"""
     icon = """"""
-    Path(os.getcwd() + "/config.conf").touch()
     Path(os.getcwd() + "/leftpanel.png").touch()
     Path(os.getcwd() + "/icon.png").touch()
-    with open(Path(os.getcwd() + "/config.conf"), "w") as f:
-        f.write(config)
     with open(Path(os.getcwd() + "/leftpanel.png"), "w") as f:
         f.write(leftPanel)  
     with open(Path(os.getcwd() + "/icon.png"), "w") as f:
@@ -35,6 +32,9 @@ def ui(name):
     elif name == "failInstall":
         return """"""
 
+    elif name == "install":
+        return """"""
+
 programData = """"""
 
 def install(dest, programData, window):
@@ -46,7 +46,10 @@ def install(dest, programData, window):
 
 class api:
     def __init__(self):
-        
+        window.get_element('#title').text = "Welcome to " + config["name"] + "Installer"
+        window.get_element('#desc').text = config["body"]
+        window.get_element('#author').text = config["author"]
+        window.get_element('#version').text = config["version"]
 
     def acceptTOS(self, yn):
         if yn == "yes":
@@ -59,18 +62,33 @@ class api:
 
     def setPath(self):
         window.load_html(ui("changePath"))
+        window.get_element('#path').text = config["path"]
 
     def finishInstall(self):
         window.load_html(ui("finishInstall"))
+        window.get_element('#desc').text = config["finish"]
+        window.get_element('#title').text = config["name"]
+        window.get_element('#version').text = config["version"]
+        window.get_element('#author').text = config["author"]
 
     def TOS(self):
         window.load_html(ui("TOS"))
+        window.get_element('#tos').text = config["tos"]
+        window.get_element('#title').text = config["name"]
 
     def failInstall(self):
         window.load_html(ui("failInstall"))
+        window.get_element('#desc').text = config["fail"]
+        window.get_element('#title').text = config["name"]
+        window.get_element('#version').text = config["version"]
+        window.get_element('#author').text = config["author"]
 
     def startUp(self):
         window.load_html(ui("startUp"))
+        window.get_element('#title').text = "Welcome to " + config["name"] + "Installer"
+        window.get_element('#desc').text = config["body"]
+        window.get_element('#author').text = config["author"]
+        window.get_element('#version').text = config["version"]
 
 if __name__ == "__main__":
     Api = api()
