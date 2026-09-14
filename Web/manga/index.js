db51490b83c3ac151dce29287fa0fd076afab339
Akari_VT <astilcodex6996@gmail.com>
2026-09-14 22:42:14 +0100

    s

--- Web/manga/index.js
+++ Web/manga/index.js
@@ -1,7 +1,7 @@
 function toggleOn() {
     document.getElementById("container-3").style.display = "block";
     document.getElementById("toggle").setAttribute("onchange", "toggleOff()");
-}
+};
 function toggleOff() {
     document.getElementById("container-3").style.display = "none";
     document.getElementById("toggle").setAttribute("onchange", "toggleOn()");
